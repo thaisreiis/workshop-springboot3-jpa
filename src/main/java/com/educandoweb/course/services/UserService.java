227661cc20166dbfca99dd87f61d99d3e20f91f0
Thais Reis <tdrv05@outlook.com>
2022-11-24 08:58:34 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -24,6 +24,15 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	
+	
+	
+	
 }
 
 
